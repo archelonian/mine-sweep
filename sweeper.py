1516d50f7457b161dd8287b38a4068131097c44e
Bonnie Chen <boc016@ucsd.edu>
2019-09-02 22:08:49 -0700

    fix indexing and add row and column indicators

--- sweeper.py
+++ sweeper.py
@@ -2,17 +2,21 @@
 # Description: Minesweeper in the command line.
 
 import random
+import string
+import math
 
 # start with preset beginner size
-ROWS = 9
-COLS = 9
-MINES = 10
+ROWS = 16
+# at current, columns <= 52 due to character limitations...
+COLS = 30
+MINES = 99
 
 # initialize initial board with -1 (denoting nothing known)
-known_board = [[1 for i in range(0, COLS)] for j in range(0, ROWS)]
+# want to do board[row][col]
+known_board = [[-1 for i in range(0, COLS)] for j in range(0, ROWS)]
 
 # initialize true board configuration as -1 as well
-true_board = [[1 for i in range(0, COLS)] for j in range(0, ROWS)]
+true_board = [[-1 for i in range(0, COLS)] for j in range(0, ROWS)]
 
 # choose spaces to set mines on
 def set_mines():
@@ -26,15 +30,19 @@ def set_mines():
 
     # reformat into board shape
     for i in range(0, len(spaces)):
-        # true_board[row][col]
-        true_board[i // ROWS][i % COLS] = spaces[i]
+        # floor divide to get how many rows down, what's left (mod) is col
+        true_board[i // COLS][i % COLS] = spaces[i]
 
 # print_board() outputs the currently known state of the game board.
 def print_board():
     output = ""
 
+    output += make_col_ids()
+
     for i in range(0, ROWS):
-        line = ""
+        # add row index, incremented by one for 1-indexed addressing
+        line = str(i + 1).rjust(int(math.log10(COLS) + 1))
+        line += "|"
 
         for j in range(0, COLS):
             curr_space = known_board[i][j]
@@ -47,7 +55,7 @@ def print_board():
                 line += " "
             # flag
             elif curr_space == 9:
-                line += "!"
+                line += "#"
             else:
                 line += str(curr_space)
 
@@ -63,6 +71,32 @@ def print_board():
     output.strip()
     print(output)
 # -- end print_board
+
+# makes the column ids and delimiting line that are printed before the board
+def make_col_ids():
+    # total width of column indices
+    # number of digits + | + (number of columns * 2) - 1 from last space
+    width = (int(math.log10(COLS)) + 1) + 1 + (2 * COLS) - 1
+
+    # use A-Z then a-z
+    col_letters = string.ascii_uppercase + string.ascii_lowercase
+    col_letters = col_letters[:COLS]
+
+    # separate letters with spaces
+    col_spaces = " " * COLS
+    col_ids = "".join(c1 + c2 for c1, c2 in zip(col_letters, col_spaces))
+
+    # strip trailing space to line up properly with final column
+    output = col_ids.strip().rjust(width) + "\n"
+
+    # delimiting line of all -----------
+    # subtract one to correspond with trailing space stripped above
+    output += ("-" * (len(col_ids) - 1)).rjust(width)
+
+    output += "\n"
+
+    return output
+# -- end make_col_ids
 
 set_mines()
 print_board()
